ab0999c47c0b2bc8d70403f410cb5212509a4751
rlagudtn <rlagudtn4510@naver.com>
2023-12-28 21:30:59 +0900

    chore : project init

---
+++ src/main/java/soo/investcrafter/InvestCrafterApplication.java
@@ -0,0 +1,13 @@
+package soo.investcrafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestCrafterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soo/investcrafter/InvestCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package soo.investcrafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
