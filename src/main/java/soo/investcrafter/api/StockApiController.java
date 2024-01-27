90006076e8a35da774803a23f25c08fde7a00075
rlagudtn <rlagudtn4510@naver.com>
2024-01-27 10:44:05 +0900

    feat(stock) : Add api frame about stock

--- src/main/java/soo/investcrafter/api/StockApiController.java
+++ src/main/java/soo/investcrafter/api/StockApiController.java
@@ -1,18 +1,29 @@
 package soo.investcrafter.api;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/home")
+@RequestMapping(value = "/stocks")
 public class StockApiController {
 
     @GetMapping(value = "")
-    public ResponseEntity readKeyIndicators() {
+    public ResponseEntity readAllStocks() {
 
         return null;
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity readStockById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping(value="/bookmarks/{id}")
+    public ResponseEntity bookmarkCompany(@PathVariable Long id) {
+        return null;
+    }
+
 
 }
