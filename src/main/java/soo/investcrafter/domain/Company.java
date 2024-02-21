9e2660a002cf9fa9de9bf98152e881a25b6a1a23
rlagudtn <rlagudtn4510@naver.com>
2024-02-21 16:55:37 +0900

    feat(entity): Update Company entity's property with Base entity

--- src/main/java/soo/investcrafter/domain/Company.java
+++ src/main/java/soo/investcrafter/domain/Company.java
@@ -1,15 +1,32 @@
 package soo.investcrafter.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity
-public class Company {
+@Getter
+@Table(name = "company")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Company extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
     private Long id;
 
     private String name;
 
-    private String code;
+    @Column(length = 64)
+    private String symbol;
 
+    @Column(length = 64)
+    private String industry;
+
+    @Column(length = 16)
+    private String exchange;
+
+    @Column(length = 64)
+    private String country;
 }
