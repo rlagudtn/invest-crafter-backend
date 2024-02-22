16ff1ff5b5b2286e9c525be4ae6e3637343e50da
rlagudtn <rlagudtn4510@naver.com>
2024-02-22 15:30:23 +0900

    feat(entity): Add key indicator entity

--- src/main/java/soo/investcrafter/domain/KeyIndicator.java
+++ src/main/java/soo/investcrafter/domain/KeyIndicator.java
@@ -1,14 +1,41 @@
 package soo.investcrafter.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class KeyIndicator {
+@Getter
+@Table(name = "key_indicator")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class KeyIndicator extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "key_indicator_id")
     private Long id;
+
+    private Short calendarYear;
+    private Float peg;
+    private Float debtRatio;
+
+    private Float roeRecent1yr;
+    private Float roeRecent2yr;
+    private Float roeRecent3yr;
+
+    private Float opmRecent1yr;
+    private Float opmRecent2yr;
+    private Float opmRecent3yr;
+
+    private Long freeCashFlow;
+    private Float dividendYield;
+
+    private Float pbr;
+    private Float per;
+
+    private Long operatingActivitiesCashFlow;
+    private Long investingActivitiesCashFlow;
+    private Long financingActivitiesCashFlow;
+
 }
 
