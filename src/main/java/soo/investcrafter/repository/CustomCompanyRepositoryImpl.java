fa2a5a3d467ae2e6d29bcc1c1790185e0e7dfb4a
rlagudtn <rlagudtn4510@naver.com>
2024-02-27 08:08:41 +0900

    feat(repository): Add query finding all companies with key-indicator through queryDSL

--- src/main/java/soo/investcrafter/repository/CustomCompanyRepositoryImpl.java
+++ src/main/java/soo/investcrafter/repository/CustomCompanyRepositoryImpl.java
@@ -5,8 +5,13 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.query.criteria.JpaSubQuery;
 import soo.investcrafter.domain.Company;
+import soo.investcrafter.domain.QCompany;
+import soo.investcrafter.domain.QKeyIndicator;
 
 import java.util.List;
+
+import static soo.investcrafter.domain.QCompany.company;
+import static soo.investcrafter.domain.QKeyIndicator.keyIndicator;
 
 @RequiredArgsConstructor
 public class CustomCompanyRepositoryImpl implements CustomCompanyRepository {
@@ -15,20 +20,12 @@ public class CustomCompanyRepositoryImpl implements CustomCompanyRepository {
 
     @Override
     public List<Company> findAllCompaniesWithLatestKeyIndicator() {
-//        List<CompanyKeyIndicatorDTO> results = queryFactory
-//                .select(Projections.constructor(CompanyKeyIndicatorDTO.class,
-//                        company.id.as("companyId"),
-//                        company.name.as("companyName"),
-//                        keyIndicator.id.as("keyIndicatorId"),
-//                        keyIndicator.calendarYear.as("calendarYear")))
-//                .from(company)
-//                .join(company.keyIndicators, keyIndicator)
-//                .where(keyIndicator.calendarYear.eq(
-//                        JPAExpressions.select(keyIndicator.calendarYear.max())
-//                                .from(keyIndicator)
-//                                .where(keyIndicator.company.eq(company))
-//                ))
-//                .fetch();
-        return null;
+        List<Company> companies = queryFactory.selectFrom(company)
+                .innerJoin(company.keyIndicators, keyIndicator)
+                .fetchJoin().fetch();
+        System.out.println("length >>>");
+        System.out.println(companies.size());
+        return companies;
+
     }
 }
